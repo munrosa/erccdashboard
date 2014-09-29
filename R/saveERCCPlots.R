1f3d188ce21076fc1c10fbe8f5da85455f970131
s.munro <s.munro@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-09-29 03:26:09 +0000

    updated CITATION and saveERCCPlots
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/erccdashboard@94643 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/saveERCCPlots.R
+++ R/saveERCCPlots.R
@@ -1,19 +1,20 @@
 #' Save erccdashboard plots to a pdf file
 #'
-#' @param exDat     list, contains input data and stores analysis results
-#' @param plotsPerPg string, if "manuscript" then the 4 main plots are printed  
-#'                   to one page, if "single" then each plot is printed to page 
-#'                   in the pdf file
-#' @param saveas     Choose file format from "pdf", "jpeg" or "png"
-#' @param res        Choose the file resolution                             
-#' @param plotlist   list, contains plots to print
+#' @param exDat         list, contains input data and stores analysis results
+#' @param plotsPerPg    string, if "main" then the 4 main plots are printed  
+#'                      to one page, if "single" then a single plot is printed 
+#'                      per page from the plotlist argument
+#' @param saveas    Choose file format from "pdf", "jpeg" or "png"
+#' @param outName   Choose output file name, default will be fileName from exDat
+#' @param res       Choose the file resolution                             
+#' @param plotlist  list, contains plots to print
 #' 
 #' @description
 #' The function savePlots will save selected figures to a pdf file. The default 
 #' is the 4 manuscript figures to a single page (plotsPerPg = "manuscript"). 
 #' If plotsPerPg = "single" then each plot is placed on an 
-#' individual page in one pdf file. If plotlist is not defined (plotlist = NULL)
-#'  then all plots in exDat$Figures are printed to the file.
+#' individual page. If plotlist is not defined (plotlist = NULL) or 
+#' if plotlist = exDat$Figures then all plots in exDat$Figures are printed to a PDF file.
 #' 
 #' @examples
 #' \donttest{
@@ -31,99 +32,90 @@
 #'
 #' exDat <- geneExprTest(exDat)
 #' 
+#' exDat <- erccROC(exDat)
+#' 
 #' exDat <- estLODR(exDat, kind="ERCC", prob=0.9)
 #' 
 #' exDat <- annotLODR(exDat)
 #' 
-#' #to print 4 plots from manuscript to a single page pdf file
-#' saveERCCPlots(exDat, plotsPerPg = "manuscript")
+#' #to print 4 main plots to a single page pdf file
+#' saveERCCPlots(exDat, plotsPerPg = "manuscript",saveas = "pdf")
+#' 
+#' #to print 4 plots to a jpeg file
+#' saveERCCPlots(exDat, plotsPerPg = "manuscript",saveas = "jpeg")
 #' 
 #' # or to create a multiple page pdf of all plots produced
 #' saveERCCPlots(exDat, plotsPerPg = "single", plotlist = exDat$Figures)
+#' 
+#' # or to create a multiple page pdf of just 2 plots
+#' saveERCCPlots(exDat, plotsPerPg = "single", 
+#'               plotlist = list(exDat$Figures$lodrPlot, exDat$Figures$maPlot))
+#' 
 #' }
 #' 
 #' 
 #' @export
 
-saveERCCPlots<-function(exDat,plotsPerPg = "manuscript", saveas = "pdf", 
-                        res = 180, plotlist = NULL){
+saveERCCPlots<-function(exDat, plotsPerPg = "main", saveas = "pdf", outName,
+                        plotlist, res){
     # Options are either the default of printing the plots as shown in 
-    ## publication (plotsPerPg = "manuscript" and plotlist is NULL) or 
-    # to print plots one per page choose (plotsPerPg = "single" and provide any
+    # the erccdashboard 2014 Nature Communications publication 
+    # (plotsPerPg = "main" and plotlist is NULL) or 
+    # to print plots one per page choose plotsPerPg = "single" and provide any
     # list of plots as the plotlist arguement
     
-    
-    # Open PDF file to write results
-    outName <- exDat$sampleInfo$filenameRoot 
-    #   if (plotsPerPg == "manuscript"){
-    #     cols = 2
-    #     pwidth = 7*cols
-    #     pheight = 7*6/cols
-    #     pdf(filename = paste(outName,"pdf",sep="."), 
-    #         width=pwidth,height = pheight)
-    #     
-    #     multiplot(exDat$Figures$rocPlot,exDat$Figures$dynRangePlot, 
-    #               exDat$Figures$lodrERCCPlot,exDat$Figures$rangeResidPlot, 
-    #               exDat$Figures$dispPlot,exDat$Figures$maPlot,cols=2)
-    #     dev.off()
-    #   } 
-    
-    cat(paste0("\nSaving main dashboard plots to ",saveas," file...\n"))
-    if (plotsPerPg == "manuscript"){
+    if(missing(plotsPerPg)){
+        plotsPerPg <- "main"
+        cat(paste0("\nSaving main dashboard plots to ",saveas," file...\n"))
+    }
+    if(missing(saveas)){
+        saveas <- "pdf"
+        cat("Printing to PDF...")
+    }
+    if(missing(outName)){
+        outName <- exDat$sampleInfo$filenameRoot
+    }
+    if(missing(res)) res = 200
+    if (plotsPerPg == "main"){
         cols = 2
         nFigs = 4
         pwidth = 7*cols
         pheight = 7*nFigs/cols
-        if (saveas == "png"){
-            png(filename = paste(outName,"png",sep="."),
-                width=pwidth,height = pheight, units = "in", res = res)
-            multiplot(exDat$Figures$dynRangePlot, exDat$Figures$rocPlot,
-                      exDat$Figures$maPlot, exDat$Figures$lodrERCCPlot, cols=cols)
-            dev.off()
-        }
-        if (saveas == "pdf"){
-            pdf(file = paste(outName,"pdf",sep="."),
-                width=pwidth,height = pheight)
-            multiplot(exDat$Figures$dynRangePlot, exDat$Figures$rocPlot,
-                      exDat$Figures$maPlot, exDat$Figures$lodrERCCPlot, cols=cols)
-            dev.off()
-        }
-        if (saveas == "jpeg"){
-            jpeg(filename = paste(outName,"jpeg",sep="."),
-                width=pwidth,height = pheight, units = "in", res = res)
-            multiplot(exDat$Figures$dynRangePlot, exDat$Figures$rocPlot,
-                      exDat$Figures$maPlot, exDat$Figures$lodrERCCPlot, cols=cols)
-            dev.off()
-        }
-        else{
-            stop(cat("Choose 'pdf', 'png', or 'jpeg' file formats"))
-        }
+        switch(saveas,
+               png = png(filename = paste(outName,"png",sep="."),
+                         width=pwidth,height = pheight,
+                         units = "in", res = res),
+               jpeg = jpeg(filename = paste(outName,"jpeg",sep="."),
+                           width=pwidth,height = pheight, 
+                           units = "in", res = res),
+               pdf = pdf(file = paste(outName,"pdf",sep="."),
+                         width=pwidth,height = pheight),
+               stop(cat("\"saveas\" = ",saveas,
+                        ", it must be \"pdf\", \"png\", or \"jpeg\"")))
+        multiplot(exDat$Figures$dynRangePlot, exDat$Figures$rocPlot,
+                  exDat$Figures$maPlot, exDat$Figures$lodrERCCPlot, cols=cols)
+        dev.off()
     }
     if (plotsPerPg == "single"){
-        if (is.null(plotlist)){
+        pwidth = 7
+        pheight = 7
+        if (missing(plotlist)){
             plotlist = exDat$Figures
-        } 
-        if (saveas == "png"){
-            png(filename = paste(outName,"png",sep="."),width=7,
-                height = 7, units = "in", res =res)
-            print(plotlist)
-            dev.off()
         }
-        if (saveas == "pdf"){
-            pdf(file = paste(outName,"pdf",sep="."),width=7,
-                height = 7)
-            print(plotlist)
-            dev.off()    
-        }
-        if (saveas == "jpeg"){
-            jpeg(filename = paste(outName,"jpeg",sep="."),width=7,
-                height = 7, units = "in", res =res)
-            print(plotlist)
-            dev.off()
-        }
-        else{
-            stop(cat("Choose 'pdf', 'png', or 'jpeg' file formats"))
-        }
-        
+        if(is.list(plotlist)){           
+            nPlotsSum <- dim(summary(plotlist))[1]
+            nPlotsLength <- length(plotlist)
+            if(nPlotsSum == nPlotsLength){
+                if(saveas != "pdf") cat("Ignoring \"saveas\" = \"",saveas,"\"\n")
+                cat("Printing plots to multiple pages in one PDF file.\n")
+                pdf(file=paste(outName,"pdf",sep="."),
+                    onefile=TRUE,width=pwidth,
+                    height=pheight)
+                print(plotlist)
+                dev.off()
+            }
+        }else{
+            stop(cat("\"plotlist\" is not a list! ")) 
+        }    
     } 
 }
